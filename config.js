2d0946afb6c0655a9140965383788f62b4f824f3
Minnn31 <108522385+Minnn31@users.noreply.github.com>
2022-07-01 22:53:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,14 @@ const CONFIG = {
     Mỉm cười với tháng 7
     Chào cuộc đời an nhiên`,
     btnIntro: '^^HiHi^^',
-    title: 'Chúc cậu sinh nhật vui vẻ nhé 🥰',
+    title: 'Chúc cậu sinh nhật vui vẻ ạ 🥰',
     desc: 'Tớ biết cậu đang cười nè ^^',
     btnYes: 'Cười lên nào <33',
     btnNo: 'Không nha :3',
     question: 'Có thể cậu sẽ làm được rất nhiều điều giữa năm tháng dài rộng phía trước, nhưng sẽ có 1 việc cậu ko làm đc <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Đó là cậu sẽ không bao giờ cô đơn :3',
-    mess: 'Chúc cậu sẽ làm được điều mình mơ ước 🥰. Dù sau này tớ có không còn cạnh cậu, tớ cũng luôn mong cậu được hạnh phúc và bình an nhé 😘😘',
+    mess: 'Chúc cậu sẽ làm được tất cả những điều mình mơ ước 🥰. Sau này có khó khăn ra sao hay bất kỳ ai bên cạnh cậu, tớ cũng luôn mong cậu được hạnh phúc và bình an nhé 😘😘',
     messDesc: 'Sinh nhật vui vẻ nha, chú mèo béo ^^',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //
-}
+    messLink: 
